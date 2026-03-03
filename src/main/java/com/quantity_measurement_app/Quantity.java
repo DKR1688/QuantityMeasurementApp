333173b30c72e77f611435a7900cf18277184ce6
DKR1688 <t.deepakrajput1688@gmail.com>
2026-03-03 08:06:53 +0530

    [Deepak] Add.Added UC12 subtraction and division operation

--- src/main/java/com/quantity_measurement_app/Quantity.java
+++ src/main/java/com/quantity_measurement_app/Quantity.java
@@ -59,6 +59,68 @@ public final class Quantity<U extends IMeasurable> {
         return new Quantity<>(round(finalValue), targetUnit);
     }
     
+    // UC12 ---
+    // subtraction
+    public Quantity<U> subtract(Quantity<U> other) {
+        return subtract(other, this.unit);
+    }
+    
+    // it subtracts another quantity from this quantity and ans will be returned in target unit
+    public Quantity<U> subtract(Quantity<U> other, U targetUnit) {
+        if (other == null) {
+        	throw new IllegalArgumentException("Other quantity cannot be null");
+        }
+
+        if (targetUnit == null) {
+        	throw new IllegalArgumentException("Target unit cannot be null");
+        }
+
+        if (!Double.isFinite(this.value) || !Double.isFinite(other.value)) {
+        	throw new IllegalArgumentException("Values must be finite numbers");
+        }
+
+        if (!this.unit.getClass().equals(other.unit.getClass())) {
+        	throw new IllegalArgumentException("Cross-category subtraction not allowed");
+        }
+
+        // converting into base Unit
+        double thisBase = this.unit.convertToBaseUnit(this.value);
+        double otherBase = other.unit.convertToBaseUnit(other.value);
+
+        double baseResult = thisBase - otherBase;
+
+        // converting into target Unit
+        double resultValue = targetUnit.convertFromBaseUnit(baseResult);
+        resultValue = Math.round(resultValue * 100.0) / 100.0;
+
+        return new Quantity<>(resultValue, targetUnit);
+    }
+    
+    // it divides this quantity by another quantity to return a dimensionless ans
+    public double divide(Quantity<U> other) {
+        if (other == null) {
+        	throw new IllegalArgumentException("Other quantity cannot be null");
+        }
+
+        if (!Double.isFinite(this.value) || !Double.isFinite(other.value)) {
+        	throw new IllegalArgumentException("Values must be finite numbers");
+        }
+
+        if (!this.unit.getClass().equals(other.unit.getClass())) {
+        	throw new IllegalArgumentException("Cross-category division not allowed");
+        }
+
+        double otherBase = other.unit.convertToBaseUnit(other.value);
+        if (otherBase == 0.0) {
+        	throw new ArithmeticException("Division by zero not allowed");
+        }
+
+        double thisBase = this.unit.convertToBaseUnit(this.value);
+        double result = thisBase / otherBase;
+
+        return result;
+    }
+    
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
--- src/main/java/com/quantity_measurement_app/QuantityMeasurementApp.java
+++ src/main/java/com/quantity_measurement_app/QuantityMeasurementApp.java
@@ -2,7 +2,8 @@ package com.quantity_measurement_app;
 
 public class QuantityMeasurementApp {
 	// UC10 ---
-	// this replaces multiple category-specific demonstration methods from UC9 with a single generic implementation.
+	// this replaces multiple category-specific demonstration methods from UC9 with
+	// a single generic implementation.
 	// this eliminates duplicate demo logic
 	public static <U extends IMeasurable> void demonstrateEquality(Quantity<U> q1, Quantity<U> q2) {
 		System.out.println("Input: " + q1 + " and " + q2);
@@ -22,6 +23,45 @@ public class QuantityMeasurementApp {
 		System.out.println();
 	}
 
+	private static void demonstrateSubtraction() {
+		System.out.println("Subtraction for implicit target unit -----");
+		Quantity<LengthUnit> length1 = new Quantity<>(10.0, LengthUnit.FEET);
+		Quantity<LengthUnit> length2 = new Quantity<>(6.0, LengthUnit.INCHES);
+	    System.out.println("Input: " + length1 + " - " + length2);
+	    System.out.println("Result: " + length1.subtract(length2));
+	    System.out.println();
+
+		Quantity<WeightUnit> weight1 = new Quantity<>(10.0, WeightUnit.KILOGRAM);
+		Quantity<WeightUnit> weight2 = new Quantity<>(5000.0, WeightUnit.GRAM);
+	    System.out.println("Input: " + weight1 + " - " + weight2);
+	    System.out.println("Result: " + weight1.subtract(weight2));
+	    System.out.println();
+
+		System.out.println("Subtraction for explicit target unit) -----");    
+		System.out.println("Input: " + length1 + " - " + length2 + " (INCHES)");
+	    System.out.println("Result: " + length1.subtract(length2, LengthUnit.INCHES));
+	    System.out.println();
+	}
+
+	private static void demonstrateDivision() {
+		System.out.println("Division -----");
+		Quantity<LengthUnit> l1 = new Quantity<>(10.0, LengthUnit.FEET);
+		Quantity<LengthUnit> l2 = new Quantity<>(2.0, LengthUnit.FEET);    
+		System.out.println("Input: " + l1 + " / " + l2);
+	    System.out.println("Result: " + l1.divide(l2));
+	    System.out.println();
+
+		Quantity<LengthUnit> l3 = new Quantity<>(24.0, LengthUnit.INCHES);
+		Quantity<LengthUnit> l4 = new Quantity<>(2.0, LengthUnit.FEET);    
+		System.out.println("Input: " + l3 + " / " + l4);
+	    System.out.println("Result: " + l3.divide(l4));
+	    System.out.println();
+	    
+	    Quantity<VolumeUnit> v1 = new Quantity<>(5.0, VolumeUnit.LITRE);
+	    Quantity<VolumeUnit> v2 = new Quantity<>(10.0, VolumeUnit.LITRE);
+	    System.out.println("Result < 1: " + v1.divide(v2));
+	}
+
 	public static void main(String[] args) {
 		// for length
 		Quantity<LengthUnit> l1 = new Quantity<>(1.0, LengthUnit.FEET);
@@ -38,7 +78,7 @@ public class QuantityMeasurementApp {
 		demonstrateEquality(w1, w2);
 		demonstrateConversion(w1, WeightUnit.GRAM);
 		demonstrateAddition(w1, w2, WeightUnit.KILOGRAM);
-		
+
 		// for volume
 		// UC11 ---
 		Quantity<VolumeUnit> v1 = new Quantity<>(1.0, VolumeUnit.LITRE);
@@ -48,5 +88,10 @@ public class QuantityMeasurementApp {
 		demonstrateEquality(v1, v2);
 		demonstrateConversion(v3, VolumeUnit.LITRE);
 		demonstrateAddition(v1, v2, VolumeUnit.LITRE);
+		
+		// UC12 ---
+	    demonstrateSubtraction();
+	    demonstrateDivision();
+
 	}
 }
