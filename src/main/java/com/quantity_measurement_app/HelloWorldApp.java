845de8f6183a9bfe67e70da03dfe4c36e6d7d114
DKR1688 <t.deepakrajput1688@gmail.com>
2026-04-07 16:05:44 +0530

    [Deepak] Add.Added .gitignore

--- src/main/java/com/quantity_measurement_app/HelloWorldApp.java
+++
@@ -1,7 +0,0 @@
-package com.quantity_measurement_app;
-
-public class HelloWorldApp {
-    public static void main(String[] args) {
-        System.out.println("Hello, QuantityMeasurementApp...");
-    }
-}
